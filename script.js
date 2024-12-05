74580bffc1ec91663ed72fa7fc6d481d8d844ac5
keerthana4276 <keerthana4276@gmail.com>
2024-12-05 18:06:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript to toggle the menu for the hamburger icon
+function toggleMenu() {
+    const navLinks = document.getElementById("nav-links");
+    navLinks.classList.toggle("show");
+  }
+  
